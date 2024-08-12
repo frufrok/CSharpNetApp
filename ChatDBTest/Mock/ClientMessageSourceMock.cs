479baac2c48c4881c5d01e21888edbbf2d3b4d8e
frufrok <frufrok@gmail.com>
2024-08-12 04:35:52 +0300

    Добавлены тесты для клиента.

---
+++ ChatDBTest/Mock/ClientMessageSourceMock.cs
@@ -0,0 +1,33 @@
+﻿using ChatDBNet.Interfaces;
+using ChatDBNet.Message;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatDBTest.Mock
+{
+    public class ClientMessageSourceMock : IMessageSource
+    {
+        public BlockingCollection<(NetMessage, IPEndPoint)> InBox { get; init; } = [];
+        public List<(NetMessage, IPEndPoint)> OutBox { get; init; } = [];
+
+        public async Task SendAsync(NetMessage message, IPEndPoint receiver)
+        {
+            OutBox.Add((message, receiver));
+        }
+
+        public async Task StartReceivingAsync()
+        {
+
+        }
+
+        public void StopReceiving()
+        {
+
+        }
+    }
+}
